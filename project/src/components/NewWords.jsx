df821a6aa95e3d2dc4d6d6ae6296b3b32caa5af2
ssstvch <alexa.stvch@yandex.ru>
2021-12-28 23:42:23 +0300

    save input's data

--- project/src/components/NewWords.jsx
+++ project/src/components/NewWords.jsx
@@ -1,124 +1,68 @@
-import React from 'react';
-import '../styles/newWords.scss';
-import { 
-    Container, 
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
-    Paper,
-    IconButton,
-    TextField,
+import React from "react";
+import "../styles/newWords.scss";
+import {
+  Container,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
+import WordRow from "./WordRow";
+import { words } from "../data/words";
 
-} from '@mui/material';
-import WordRow from './WordRow';
-// import { words } from '../data/words';
+const tableCellStyle = [{ width: "9vw", p: "1.2vw 0.7vw" }];
+const tableCellBigStyle = [{ width: "15vw", p: "1.2vw 0.7vw" }];
 
-const words = [
-    {
-        id:"00000",
-        word:"butterfly",
-        transcription:"[ ˈbʌtəflaɪ ]",
-        translate:"бабочка",
-        explanation: "Beautiful insect with brighty colored wings. Feed on flower nectar or fruit juice.",
-        theme:"Насекомые"
-    },
-    {
-        id:"00001",
-        word:"hedgehog",
-        transcription:"[ˈhedʒhɔːɡ]",
-        translate:"ёжик",
-        explanation: " ",
-        theme:"Животные"
-    },
-    {
-        id:"00002",
-        word:"apple",
-        transcription:"[ˈæpl]",
-        translate:"яблоко",
-        explanation: " ",
-        theme:"Фрукты"
-    },
-    {
-        id:"00003",
-        word:"pear",
-        transcription:"[peə]",
-        translate:"груша",
-        explanation: " ",
-        theme:"Фрукты"
-    },
-    {
-        id:"00004",
-        word:"carrot",
-        transcription:"[ˈkærət]",
-        translate:"морковка",
-        explanation: " ",
-        theme:"Овощи"
-    },
-    {
-        id:"00005",
-        word:"plum",
-        transcription:"[plʌm]",
-        translate:"слива",
-        explanation: " ",
-        theme:"Фрукты"
-    },
-    {
-        id:"00006",
-        word:"unicorn",
-        transcription:"",
-        translate:"единорог",
-        explanation: " ",
-        theme:"Животные"
-    },
-    {
-        id:"00007",
-        word:"cat",
-        transcription:"[kæt]",
-        translate:"кот",
-        explanation: " ",
-        theme:"Животные"
-    },
-]
+const NewWords = () => {
+  return (
+    <Container sx={{ mt: "2vw" }} component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell sx={tableCellStyle}>
+              <b>Word</b>
+            </TableCell>
+            <TableCell sx={tableCellStyle}>
+              <b>Translate</b>
+            </TableCell>
+            <TableCell sx={tableCellStyle}>
+              <b>Transcription</b>
+            </TableCell>
+            <TableCell sx={tableCellStyle}>
+              <b>Theme</b>
+            </TableCell>
+            <TableCell sx={tableCellBigStyle}>
+              <b>Explanation</b>
+            </TableCell>
+            <TableCell sx={{ width: "6vw" }}>{` `}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {words.map((word) => {
+            console.log(word);
+            return (
+              <WordRow
+                key={word.id}
+                id={word.id}
+                word={word.word}
+                translate={word.translate}
+                transcription={word.transcription}
+                theme={word.theme}
+                explanation={word.explanation}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
 
-const tableCellStyle = [{width:'9vw', p:'1.2vw 0.7vw'}];
-const tableCellBigStyle = [{width:'15vw', p:'1.2vw 0.7vw'}];
-
-const NewWords = () => (
-    <React.Fragment>
-        <Container sx={{ mt: "2vw" }} component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={tableCellStyle}><b>Word</b></TableCell>
-                        <TableCell sx={tableCellStyle}><b>Translate</b></TableCell>
-                        <TableCell sx={tableCellStyle}><b>Transcription</b></TableCell>
-                        <TableCell sx={tableCellStyle}><b>Theme</b></TableCell>
-                        <TableCell sx={tableCellBigStyle}><b>Explanation</b></TableCell>
-                        <TableCell sx={{width: "6vw"}}>{` `}</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {words.map((word) => {
-                        console.log(word);
-                        return (<WordRow
-                            key={word.id}
-                            word={word.word}
-                            translate={word.translate}
-                            transcription={word.transcription}
-                            theme={word.theme}
-                            explanation={word.explanation} 
-                        />);
-                    })}
-                </TableBody>
-            </Table>
-        </Container>
-    </React.Fragment>
-)
-
-export default NewWords
-{/* 
+export default NewWords;
+{
+  /* 
     <TableRow>
         <TableCell className={`tablecell`} component="th" scope="row">
             <TextField 
@@ -172,4 +116,5 @@ export default NewWords
             </Container>
         </TableCell>
     </TableRow>
-*/}
+*/
+}
--- project/src/components/WordRow.jsx
+++ project/src/components/WordRow.jsx
@@ -1,115 +1,130 @@
-import React from 'react';
-import '../styles/newWords.scss'
-import { 
-    Container, 
-    TableRow,
-    TableCell,
-    TextField,
-    IconButton,
+import React from "react";
+import "../styles/newWords.scss";
+import {
+  Container,
+  TableRow,
+  TableCell,
+  TextField,
+  IconButton,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import DoneIcon from "@mui/icons-material/Done";
 
-} from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import DoneIcon from '@mui/icons-material/Done';
+const WordRow = ({
+  word,
+  translate,
+  transcription,
+  theme,
+  explanation,
+  id,
+}) => {
+  const [redrow, setRedrow] = React.useState(false);
+  const [inputText, setInputText] = React.useState(-1);
 
+  const handleRowClick = () => {
+    setRedrow(!redrow);
+  };
 
-const WordRow = ({word, translate, transcription, theme, explanation}) => {
+  const handleChange = (e) => {
+    // достаю инпуты редактируемой строки
+    const dataset = e.target.parentNode.dataset.id;
+    let inputParent = document.getElementsByClassName(dataset);
 
-    const [redrow, setRedrow] = React.useState(false);
+    // создаю массив для хранения значений из инпутов
+    let inputs = [];
 
-    const handleRowClick = () => {
-        setRedrow(!redrow);
-    };
+    // сохраняю в массив данные из инпутов
+    for (let key of inputParent) {
+      inputs.push(key.children[0].children[0].value);
+    }
+    console.log(inputs);
 
+    // бывает, что возвращается пустой массив, проверка перед рендерингом
+    inputs.length !== 0
+      ? setInputText()
+      : console.log("Нулевой массив, нужно нажать ещё раз");
+  };
 
-    return (
-        <React.Fragment>
-                {
-                    redrow
-                    ? <TableRow>
-                        <TableCell className={`tablecell`}>
-                            <TextField 
-                                size="small"
-                                variant="standard" 
-                                defaultValue={word}
-                                className={`tablecell__input`}
-                            />
-                        </TableCell>
-                        <TableCell className={`tablecell`}  >
-                            <TextField 
-                                size="small"
-                                variant="standard" 
-                                defaultValue={translate} 
-                                className={`tablecell__input`}
-                            />
-                        </TableCell>
-                        <TableCell className={`tablecell`} >
-                            <TextField 
-                                size="small" 
-                                variant="standard" 
-                                defaultValue={transcription}
-                                className={`tablecell__input`}
-                            />
-                        </TableCell>
-                        <TableCell className={`tablecell`} >
-                            <TextField 
-                                size="small" 
-                                variant="standard" 
-                                defaultValue={theme} 
-                                className={`tablecell__input`}
-                            />
-                        </TableCell>
-                        <TableCell className={`tablecell tablecell_big`} >
-                            <TextField 
-                                size="small" 
-                                variant="standard" 
-                                multiline
-                                sx= {{width: "100%"}}
-                                defaultValue={explanation}
-                                className={`tablecell__input`}
-                            />
-                        </TableCell>
-                        <TableCell className={`tablecell__icons`} >
-                        <Container>
-                        <IconButton>
-                            <DoneIcon 
-                                onClick={handleRowClick}
-                            />
-                        </IconButton>
-                        <IconButton>
-                            <RemoveCircleIcon 
-                                onClick={handleRowClick}
-                            />
-                        </IconButton>
-                        </Container>
-                        </TableCell>
-                    </TableRow>
-                    : <TableRow>
-                        <TableCell className={`tablecell`} >{word}</TableCell>
-                        <TableCell className={`tablecell`} >{translate}</TableCell>
-                        <TableCell className={`tablecell`} >{transcription}</TableCell>
-                        <TableCell className={`tablecell`} >{theme}</TableCell>
-                        <TableCell className={`tablecell_big`} >{explanation}</TableCell>
-                        <TableCell className={`tablecell__icons`} >
-                            <Container>
-                                <IconButton>
-                                    <EditIcon 
-                                        onClick={handleRowClick}
-                                    />
-                                </IconButton>
-                                <IconButton>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </Container>
-                        </TableCell>
-                    </TableRow>
-                }
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      {redrow ? (
+        <TableRow>
+          <TableCell className={`tablecell`}>
+            <TextField
+              size="small"
+              variant="standard"
+              defaultValue={word}
+              className={`tablecell__input ${`${word}-${id}`}`}
+            />
+          </TableCell>
+          <TableCell className={`tablecell`}>
+            <TextField
+              size="small"
+              variant="standard"
+              defaultValue={translate}
+              className={`tablecell__input ${`${word}-${id}`}`}
+            />
+          </TableCell>
+          <TableCell className={`tablecell`}>
+            <TextField
+              size="small"
+              variant="standard"
+              defaultValue={transcription}
+              className={`tablecell__input ${`${word}-${id}`}`}
+            />
+          </TableCell>
+          <TableCell className={`tablecell`}>
+            <TextField
+              size="small"
+              variant="standard"
+              defaultValue={theme}
+              className={`tablecell__input ${`${word}-${id}`}`}
+            />
+          </TableCell>
+          <TableCell className={`tablecell tablecell_big`}>
+            <TextField
+              size="small"
+              variant="standard"
+              multiline
+              sx={{ width: "100%" }}
+              defaultValue={explanation}
+              className={`tablecell__input ${`${word}-${id}`}`}
+            />
+          </TableCell>
+          <TableCell className={`tablecell__icons`}>
+            <Container>
+              <IconButton data-id={`${word}-${id}`} onClick={handleChange}>
+                <DoneIcon />
+              </IconButton>
+              <IconButton data-id={`${word}-${id}`} onClick={handleRowClick}>
+                <RemoveCircleIcon />
+              </IconButton>
+            </Container>
+          </TableCell>
+        </TableRow>
+      ) : (
+        <TableRow>
+          <TableCell className={`tablecell`}>{word}</TableCell>
+          <TableCell className={`tablecell`}>{translate}</TableCell>
+          <TableCell className={`tablecell`}>{transcription}</TableCell>
+          <TableCell className={`tablecell`}>{theme}</TableCell>
+          <TableCell className={`tablecell_big`}>{explanation}</TableCell>
+          <TableCell className={`tablecell__icons`}>
+            <Container>
+              <IconButton onClick={handleRowClick}>
+                <EditIcon />
+              </IconButton>
+              <IconButton>
+                <DeleteIcon />
+              </IconButton>
+            </Container>
+          </TableCell>
+        </TableRow>
+      )}
+    </React.Fragment>
+  );
+};
 
-export default WordRow
-
-
- 
+export default WordRow;
